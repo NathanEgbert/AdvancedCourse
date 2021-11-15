83a9a479a66093f12db519d361896415424b622b
Nathan Egbert <nathan.egbert@agilethought.com>
2021-11-15 16:26:56 -0500

    Fixted test

--- AdvancedCourse/Lecture 5/Employees.cs
+++ AdvancedCourse/Lecture 5/Employees.cs
@@ -8,25 +8,15 @@ namespace AdvancedCourse.Lecture_5
 {
     public class Employees : IEmployee
     {
-        private string firstName;
-
-        private string lastName;
-
-        private string employeeId;
-
-        private string employeeType;
-
-        private string salary;
-
         private string role;
 
 
-        public string FirstName { get => firstName; set => firstName = value; }
-        public string LastName { get => lastName; set => lastName = value; }
-        public string EmployeeID { get => employeeId; set => employeeId = value; }
-        public string EmployeeType { get => employeeType; set => employeeType = value; }
-        public string Salary { get => salary; set => salary = value; }
-        public string Role { get => role; set => role = value; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmployeeID { get; set; }
+        public string EmployeeType { get; set; }
+        public string Salary { get; set; }
+        public string Role { get ; set ; }
 
         public Employees(string firstName, string lastName, string employeeId, string employeeType, string salary, string role)
         {
